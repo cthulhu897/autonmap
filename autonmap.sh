835ec293c420d65b843d4d314156c1904cb42f90
Francisco Díaz <cthulhu@diax.mx>
2024-07-15 15:40:54 -0600

    Updated to have better code readability and improved control over nmap parameters

--- autonmap.sh
+++ autonmap.sh
@@ -1,123 +1,137 @@
 #!/bin/bash
-usage () {
-  echo -e "\n[i] autonmap is designed to run a full TCP/IP host discovery, scaning and firgerprinting"
-  echo -e "[i] Usage:"
-  echo -e "sudo ./autonmap -o <output_files> -t <target>\n\n"
-  echo -e "\t-h \tDisplays this message of use"
-  echo -e "\t-o \tFile name to use to save scan related files"
-  echo -e "\t-t \tTarget IP,CIDR or pass an input file as \"-iL file.lst\" to scan more complex ranges\n\n";
-  echo -e "[i] This script is only a wrapper for bash evals, so, use parameters with care!"
+
+# Color definitions and log functions
+greenColour="\e[0;32m\033[1m"; export greenColour
+redColour="\e[0;31m\033[1m"; export redColour
+blueColour="\e[0;34m\033[1m"; export blueColour
+lightblueColour="\e[0;34m\033[1;34m"; export lightblueColour
+yellowColour="\e[0;33m\033[1m"; export yellowColour
+purpleColour="\e[0;35m\033[1m"; export purpleColour
+turquoiseColour="\e[0;36m\033[1m"; export turquoiseColour
+grayColour="\e[0;37m\033[1m"; export grayColour
+endColour="\033[0m\e[0m"; export endColour
+
+function log_error() {
+    echo -e "[${redColour}❌${endColour}] ${redColour}$1${endColour}"
+}
+function log_warning() {
+    echo -e "[${yellowColour}⚠️${endColour}] $1"
+}
+function log_success() {
+    echo -e "[${greenColour}✅${endColour}] $1"
+}
+function log_info() {
+    echo -e "[${lightblueColour}💬${endColour}] $1"
+}
+function log_debug() {
+    if [[ "${DEBUG}" == "true" ]]; then
+        echo -e "[${turquoiseColour}🕷️${endColour}] ${grayColour}$1${endColour}"
+    fi
 }
 
-# Check if root launch
-if [ "$EUID" -ne 0 ]
-  then echo -e "[!] Please run as root\n[i] check sudo ./autonmap -h"
+# Usage function
+usage() {
+  echo -e "\n${yellowColour}Usage:${endColour} sudo $0 -o <output_files> -t <target>\n"
+  echo -e "${lightblueColour}Options:${endColour}"
+  echo -e "\t-h\tDisplay this help message."
+  echo -e "\t-o FILE\tSpecify the base name for files to save scan results."
+  echo -e "\t-t TARGET\tSpecify the target IP, CIDR, or an input file with \"-iL file.lst\" for complex ranges.\n"
+  echo -e "${purpleColour}Note:${endColour} This script acts as a wrapper for Nmap commands; use parameters carefully!\n"
+}
+
+# Check for root privileges
+if [ "$EUID" -ne 0 ]; then
+  log_error "Please run as root."
+  echo "Example: sudo $0 -h"
   exit 1
 fi
 
-# Check opts
+# Parse command-line options
 while getopts ":t:o:h" opt; do
   case $opt in
-    # help
     h)
-      echo "[+] ================================================================================";
-      echo "[+] ====================================   USAGE   =================================";
-      echo "[+] ================================================================================";
       usage
-      exit 1
+      exit 0
       ;;
-
-    # Target
     t)
-      echo -e "[+] Target:\t\t $OPTARG" >&2
       TARGET="$OPTARG"
-      f_target=true
       ;;
-
-    # output file
     o)
-      echo -e "[+] Output Files:\t $OPTARG" >&2
-      NAME="$OPTARG"
-      f_name=true
+      OUTPUT="$OPTARG"
       ;;
-
     \?)
-      echo -e "[!] Invalid option:\t -$OPTARG" >&2
+      log_error "Invalid option: -$OPTARG"
       usage
       exit 1
       ;;
-
     :)
-      echo -e "[!] Option -$OPTARG requires an argument." >&2
+      log_error "Option -$OPTARG requires an argument."
       usage
       exit 1
       ;;
   esac
 done
 
-
-# check tan output and target is not empty
-if [ -z "$f_name" ] || [ -z "$f_target" ]; then
-  echo -e "[!] ERROR! \n[i] check ./autonmap -h"
-  exit 1;
+# Ensure required options are provided
+if [ -z "$TARGET" ] || [ -z "$OUTPUT" ]; then
+  log_error "Missing target or output file."
+  usage
+  exit 1
 fi
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# Define the directory to store script output
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-# if [ -d "$DIRECTORY" ]; then
-#   echo -e "[!] WARNING!"
-#   echo -e "[!] [ $(pwd)/$DIRECTORY/ ] already exists in this system."
-#   echo -e "[!] This script will overwrite existing files in the directory!\n";
-#   read -p "Continue? [Y/n] > " -n 1 -r
-#   if [[ ! $REPLY =~ ^[Yy]$ ]]
-#   then
-#     echo -e "\n[!]\t ERROR!"
-#     echo -e "[!]\t A B O R T E D !!!"
-#     exit 1;
-#   fi
-# fi
+# Begin script operations
+log_info "Running host discovery..."
+DISCOVERY_TCP_PORTS="80,443,22,3389,1723,8080,3306,135,53,143,139,445,110,25,21,23,5432,27017,1521"
+DISCOVERY_UDP_PORTS="139,53,67,135,445,1434,138,123,137,161,631"
+NMAP_HOST_DISCOVERY_CMD="nmap -vvv -sn -PE -PP -PM -PS${DISCOVERY_TCP_PORTS} -PU${DISCOVERY_UDP_PORTS} $TARGET -oA ${OUTPUT}_hosts -T4 --max-retries 3 --min-rtt-timeout 250ms --max-rtt-timeout 2000ms --initial-rtt-timeout 750ms --min-rate 125 --max-rate 2000 --min-hostgroup 256 --max-hostgroup 1024"
+if ! eval "$NMAP_HOST_DISCOVERY_CMD"; then
+  log_error "Failed to perform host discovery."
+  exit 1
+fi
 
-#init the varibles and mkdir
-#echo "[+] Creating directory for writting the output [$(pwd)/$DIRECTORY/]"
-#mkdir -p "$DIRECTORY"
+# Extract list of alive hosts
+if ! grep "Status: Up" "${OUTPUT}_hosts.gnmap" | cut -d " " -f 2 > "${OUTPUT}_alive.lst"; then
+  log_warning "No hosts found up. Exiting."
+  exit 1
+fi
 
-#Host discovery on top TCP and UDP ports
-ALIVEHOSTS="nmap -sn -PE -PP -PM -PS80,443,22,3389,1723,8080,3306,135,53,143,139,445,110,25,21,23,5432,27017,1521 -PU139,53,67,135,445,1434,138,123,137,161,631 $TARGET -oA ${NAME}_autonmap_alive";
-echo "[+] ================================================================================";
-echo "[+] ==========================  H O S T    D I S C O V E R Y  ======================";
-echo "[+] ================================================================================";
-echo "[#] > "$ALIVEHOSTS;
-echo "[+] ================================================================================";
-eval $ALIVEHOSTS
-#Extract list of alive hosts
-cat "${NAME}_autonmap_alive.gnmap" | grep "Status: Up" | cut -d " " -f 2 > "${NAME}_autonmap_alive".lst;
+# SYN scan
+log_info "Running SYN scan..."
+SYN_SCAN_TCP_PORTS="1-65535"
+SYN_SCAN_UDP_PORTS="7,9,11,13,17,19,37,49,53,67-69,80,88,111,120,123,135-139,158,161-162,177,213,259-260,427,443,445,464,497,500,514-515,518,520,523,593,623,626,631,749-751,996-999,1022-1023,1025-1030,1194,1433-1434,1645-1646,1701,1718-1719,1812-1813,1900,2000,2048-2049,2222-2223,2746,3230-3235,3283,3401,3456,3703,4045,4444,4500,4665-4666,4672,5000,5059-5061,5351,5353,5632,6429,7777,8888,9100-9102,9200,10000,17185,18233,20031,23945,26000-26004,26198,27015-27030,27444,27960-27964,30718,30720-30724,31337,32768-32769,32771,32815,33281,34555,44400,47545,49152-49154,49156,49181-49182,49186,49190-49194,49200-49201,49211,54321,65024"
+NMAP_SYN_SCAN_CMD="nmap -vvv -Pn -sS -sU -p T:${SYN_SCAN_TCP_PORTS},U:${SYN_SCAN_UDP_PORTS} -oA ${OUTPUT}_ports -iL ${OUTPUT}_alive.lst -T4 --max-retries 3 --min-rtt-timeout 250ms --max-rtt-timeout 2000ms --initial-rtt-timeout 750ms --min-rate 125 --max-rate 2000 --min-hostgroup 256 --max-hostgroup 1024 --defeat-rst-ratelimit"
+if ! eval "$NMAP_SYN_SCAN_CMD"; then
+  log_error "SYN scan failed."
+  exit 1
+fi
 
-#Fast full port scan with 65535 tcp ports and top UDP
-SYNSCAN="nmap -Pn -sS -sU -p T:1-65535,U:7,9,11,13,17,19,37,49,53,67-69,80,88,111,120,123,135-139,158,161-162,177,213,259-260,427,443,445,464,497,500,514-515,518,520,523,593,623,626,631,749-751,996-999,1022-1023,1025-1030,1194,1433-1434,1645-1646,1701,1718-1719,1812-1813,1900,2000,2048-2049,2222-2223,2746,3230-3235,3283,3401,3456,3703,4045,4444,4500,4665-4666,4672,5000,5059-5061,5351,5353,5632,6429,7777,8888,9100-9102,9200,10000,17185,18233,20031,23945,26000-26004,26198,27015-27030,27444,27960-27964,30718,30720-30724,31337,32768-32769,32771,32815,33281,34555,44400,47545,49152-49154,49156,49181-49182,49186,49190-49194,49200-49201,49211,54321,65024 -T4 --max-retries 2 --min-rtt-timeout 500ms --max-rtt-timeout 2000ms --initial-rtt-timeout 750ms --defeat-rst-ratelimit --min-rate 900 --max-rate 8000 --disable-arp-ping -oA ${NAME}_autonmap_openports -iL ${NAME}_autonmap_alive.lst -vv ";
-echo "[+] ================================================================================";
-echo "[+] ================================  S Y N   S C A N  =============================";
-echo "[+] ================================================================================";
-echo "[#] > "$SYNSCAN;
-echo "[+] ================================================================================";
-eval $SYNSCAN;
-
-#Extract open ports list trimming ending comma
-TCPPORTS=$(cat "${NAME}_autonmap_openports.gnmap" | awk -F " " '{ s = ""; for (i = 4; i <= NF; i++) s = s $i " "; print s }' | tr ", " "\n" | grep open | grep tcp | cut -d "/" -f 1 | sort -nu | paste -s -d, - );
-UDPPORTS=$(cat "${NAME}_autonmap_openports.gnmap" | awk -F " " '{ s = ""; for (i = 4; i <= NF; i++) s = s $i " "; print s }' | tr ", " "\n" | grep open | grep udp | cut -d "/" -f 1 | sort -nu | paste -s -d, - );
-
-#Final scan full connect and service reconnaissance if<>fi sentece to handle empty strings and keep command integrity
-if [ -z "${TCPPORTS}" ]; then
-    NMAPSCAN="nmap -Pn -sS -sV -sC -O -A -sU -p T:22,80,443,U:137,161,${UDPPORTS} -oA ${NAME}_autonmap_openports $TARGET ";
+# Detailed scan
+log_info "Running detailed scan..."
+if [ ! -s "${OUTPUT}_ports.gnmap" ]; then
+  log_warning "No open ports found from SYN scan. Scanning default ports..."
+  DETAIL_SCAN_PORTS="T:22,80,443"
+  DETAIL_SCAN_CMD="nmap -Pn -sT -sV -sC -O -p $DETAIL_SCAN_PORTS -oA ${OUTPUT}_final $TARGET -T2 --max-retries 3 --min-rtt-timeout 250ms --max-rtt-timeout 2000ms --initial-rtt-timeout 750ms --min-rate 125 --max-rate 2000 --min-hostgroup 256 --max-hostgroup 1024 --defeat-rst-ratelimit"
 else
-    NMAPSCAN="nmap -Pn -sS -sV -sC -O -A -sU -p T:22,80,443,${TCPPORTS},U:137,161,${UDPPORTS} -oA ${NAME}_autonmap_servicescan $TARGET ";
+  OPEN_TCP_PORTS=$(awk '/open/&&/tcp/{print int($1)}' "${OUTPUT}_ports.gnmap" | sort -u | paste -sd,)
+  OPEN_UDP_PORTS=$(awk '/open/&&/udp/{print int($1)}' "${OUTPUT}_ports.gnmap" | sort -u | paste -sd,)
+  DETAIL_SCAN_PORTS="T:22,80,443,$OPEN_TCP_PORTS,U:137,161,$OPEN_UDP_PORTS"
+  DETAIL_SCAN_CMD="nmap -Pn -sS -sV -sC -O -p $DETAIL_SCAN_PORTS -oA ${OUTPUT}_final $TARGET"
 fi
-echo "[+] ================================================================================";
-echo "[+] =============================   F U L L    S C A N   ===========================";
-echo "[+] ================================================================================";
-echo "[#] > " $NMAPSCAN;
-echo "[+] ================================================================================";
-eval $NMAPSCAN;
 
-xsltproc -o "${NAME}_autonmap_report.html" "${DIR}/nmap-bootstrap.xsl/nmap-bootstrap.xsl" "${NAME}_autonmap_servicescan.xml"
+if ! eval "$DETAIL_SCAN_CMD"; then
+  log_error "Detailed scan failed."
+  exit 1
+fi
 
-echo "[*] D O N E \n"
+# Generate report
+log_info "Generating HTML report..."
+XSLT_TRANSFORM_CMD="xsltproc -o ${OUTPUT}_report.html ${SCRIPT_DIR}/nmap-bootstrap.xsl ${OUTPUT}_final.xml"
+if ! eval "$XSLT_TRANSFORM_CMD"; then
+  log_error "Failed to generate HTML report."
+  exit 1
+fi
+
+log_success "Scan completed and report generated."
